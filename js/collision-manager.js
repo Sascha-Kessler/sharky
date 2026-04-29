e582087a702592124c3b3d0057dfa4a3aa972b6b
SaschaKessler-dev <sascha.k1809@gmail.com>
2026-04-29 17:34:11 +0200

    refactoring and jsdoc

---
+++ js/collision-manager.js
@@ -0,0 +1,129 @@
+/**
+ * Handles all collision checks inside the game world.
+ */
+class CollisionManager {
+  /**
+   * Creates a new collision manager.
+   * @param {World} world - Current game world instance
+   */
+  constructor(world) {
+    this.world = world;
+  }
+
+  /**
+   * Handles all collision types.
+   */
+  handleAll() {
+    this.handleEnemyCollisions();
+    this.handleBubbleCollisions();
+    this.handleFinSlapCollisions();
+    this.handlePoisonBottlesCollisions();
+    this.handleCoinCollisions();
+  }
+
+  /**
+   * Checks collisions between character and enemies.
+   */
+  handleEnemyCollisions() {
+    const { character, enemies } = this.world;
+
+    enemies.forEach((enemy) => {
+      if (character.isColliding(enemy)) {
+        this.world.handleCharacterHit(20);
+      }
+    });
+  }
+
+  /**
+   * Checks collisions between bubbles and enemies.
+   */
+  handleBubbleCollisions() {
+    const world = this.world;
+
+    world.throwableObjects = world.throwableObjects.filter((bubble) => {
+      const hitEnemy = world.enemies.find((enemy) => bubble.isColliding(enemy));
+
+      if (hitEnemy) {
+        hitEnemy.hit(bubble.damage);
+        world.soundManager.play("bubblePop");
+        return false;
+      }
+
+      return true;
+    });
+  }
+
+  /**
+   * Checks collisions between fin slap attack and enemies.
+   */
+  handleFinSlapCollisions() {
+    const { character, enemies } = this.world;
+
+    if (!character.isFinSlapAttacking || character.finSlapHitDone) return;
+
+    enemies.forEach((enemy) => {
+      if (this.isFinSlapHit(enemy)) {
+        enemy.finSlapMarked = true;
+        character.finSlapHitDone = true;
+      }
+    });
+  }
+
+  /**
+   * Checks if an enemy is inside the fin slap attack range.
+   * @param {MovableObject} enemy - Enemy object to check
+   * @returns {boolean}
+   */
+  isFinSlapHit(enemy) {
+    const char = this.world.character;
+    const attackOffset = 10;
+
+    const overlapsX = char.otherDirection
+      ? char.x - attackOffset < enemy.x + enemy.width
+      : char.x + char.width + attackOffset > enemy.x;
+
+    const overlapsY =
+      char.y < enemy.y + enemy.height && char.y + char.height > enemy.y;
+
+    return overlapsX && overlapsY;
+  }
+
+  /**
+   * Checks collisions between character and poison bottles.
+   */
+  handlePoisonBottlesCollisions() {
+    const world = this.world;
+
+    world.poisonBottles = world.poisonBottles.filter((poisonBottle) => {
+      if (world.character.isColliding(poisonBottle)) {
+        world.soundManager.play("bottlePickup");
+
+        world.character.poisonBottles++;
+
+        world.poisonbar.updatePoison(world.character.poisonBottles);
+
+        return false;
+      }
+
+      return true;
+    });
+  }
+
+  /**
+   * Checks collisions between character and coins.
+   */
+  handleCoinCollisions() {
+    const world = this.world;
+
+    world.coins = world.coins.filter((coin) => {
+      if (world.character.isColliding(coin)) {
+        world.soundManager.play("coinPickup");
+        world.character.coins++;
+        world.coinbar.updateCoin(world.character.coins);
+        return false;
+      }
+
+      return true;
+    });
+  }
+}
--- models/world.class.js
+++ models/world.class.js
@@ -9,15 +9,14 @@ class World {
 
   ctx;
   camera_x = 0;
-
   throwableObjects = [];
 
   /**
-   * Creates a new game world
-   * @param {HTMLCanvasElement} canvas
-   * @param {Keyboard} keyboard
-   * @param {Level} level
-   * @param {SoundManager} soundManager
+   * Creates a new game world.
+   * @param {HTMLCanvasElement} canvas - Game canvas element
+   * @param {Keyboard} keyboard - Keyboard input state
+   * @param {Level} level - Current level data
+   * @param {SoundManager} soundManager - Sound manager instance
    */
   constructor(canvas, keyboard, level, soundManager) {
     this.canvas = canvas;
@@ -25,6 +24,8 @@ class World {
     this.ctx = canvas.getContext("2d");
     this.level = level;
     this.soundManager = soundManager;
+    this.throwableObjects = [];
+    this.collisionManager = new CollisionManager(this);
 
     this.backgroundObjects = level.backgroundObjects;
     this.enemies = level.enemies;
@@ -47,7 +48,7 @@ class World {
   }
 
   /**
-   * Returns the height of the world
+   * Returns the height of the world.
    * @returns {number}
    */
   get height() {
@@ -55,21 +56,17 @@ class World {
   }
 
   /**
-   * Updates the entire game world
+   * Updates the entire game world.
    */
   update() {
     this.handleEntities();
     this.handleProjectiles();
     this.handleCollisions();
-
-    this.healthbar.updateHealth(this.character.health);
-    this.poisonbar.updatePoison(this.character.poisonBottles);
-    this.coinbar.updateCoin(this.character.coins);
-    this.updateBossHealthbar();
+    this.updateUI();
   }
 
   /**
-   * Updates all entities like character, enemies and coins
+   * Updates all entities like character, enemies and coins.
    */
   handleEntities() {
     this.character.update();
@@ -78,7 +75,7 @@ class World {
   }
 
   /**
-   * Updates all enemies and removes dead/off-screen ones
+   * Updates all enemies and removes dead/off-screen enemies.
    */
   updateEnemies() {
     this.enemies.forEach((enemy) => enemy.update());
@@ -90,7 +87,7 @@ class World {
   }
 
   /**
-   * Updates all projectile objects
+   * Updates all projectile objects.
    */
   handleProjectiles() {
     this.throwableObjects.forEach((bubble) => bubble.update());
@@ -98,7 +95,7 @@ class World {
   }
 
   /**
-   * Removes expired or distant throwable objects
+   * Removes expired or distant throwable objects.
    */
   updateThrowableObjects() {
     this.throwableObjects = this.throwableObjects.filter(
@@ -108,49 +105,36 @@ class World {
   }
 
   /**
-   * Creates and throws a new projectile
-   * @param {string} type - Type of bubble
-   */
-  throwObject(type) {
-    const x = this.character.x + (this.character.otherDirection ? -20 : 120);
-    const y = this.character.y + 80;
-
-    const bubble = new ThrowableObject(
-      x,
-      y,
-      this.character.otherDirection,
-      type,
-    );
-
-    bubble.throw();
-    this.throwableObjects.push(bubble);
-  }
-
-  /**
-   * Handles all collision checks
+   * Handles all collision checks through the collision manager.
    */
   handleCollisions() {
-    this.handleEnemyCollisions();
-    this.handleBubbleCollisions();
-    this.handleFinSlapCollisions();
-    this.handlePoisonBottlesCollisions();
-    this.handleCoinCollisions();
+    this.collisionManager.handleAll();
   }
 
   /**
-   * Checks collisions between character and enemies
+   * Updates all UI elements.
    */
-  handleEnemyCollisions() {
-    this.enemies.forEach((enemy) => {
-      if (this.character.isColliding(enemy)) {
-        this.handleCharacterHit(20);
-      }
-    });
+  updateUI() {
+    this.healthbar.updateHealth(this.character.health);
+    this.poisonbar.updatePoison(this.character.poisonBottles);
+    this.coinbar.updateCoin(this.character.coins);
+    this.updateBossHealthbar();
   }
 
   /**
-   * Handles damage taken by the character
-   * @param {number} damage - Damage amount
+   * Updates the boss health bar if a boss exists.
+   */
+  updateBossHealthbar() {
+    const boss = this.enemies.find((enemy) => enemy instanceof Endboss);
+
+    if (boss && this.bossHealthbar) {
+      this.bossHealthbar.updateBossHealth(boss.health);
+    }
+  }
+
+  /**
+   * Handles damage taken by the character.
+   * @param {number} [damage=20] - Damage amount
    */
   handleCharacterHit(damage = 20) {
     if (
@@ -159,92 +143,43 @@ class World {
     )
       return;
 
+    this.applyDamage(damage);
+  }
+
+  /**
+   * Applies damage to the character.
+   * @param {number} damage - Damage amount
+   */
+  applyDamage(damage) {
     this.soundManager.play("getsHit");
 
     this.character.lastHit = Date.now();
     this.character.health -= damage;
     this.character.hurt();
 
-    this.healthbar.updateHealth(this.character.health);
+    this.updateCharacterUI();
+    this.checkCharacterDeath();
+  }
 
+  /**
+   * Updates character-related UI elements.
+   */
+  updateCharacterUI() {
+    this.healthbar.updateHealth(this.character.health);
+  }
+
+  /**
+   * Checks if the character should die.
+   */
+  checkCharacterDeath() {
     if (this.character.health <= 0) {
       this.character.die();
     }
   }
 
   /**
-   * Checks collisions between bubbles and enemies
+   * Applies delayed fin slap damage to marked enemies.
    */
-  handleBubbleCollisions() {
-    this.throwableObjects = this.throwableObjects.filter((bubble) => {
-      const hitEnemy = this.enemies.find((enemy) => bubble.isColliding(enemy));
-
-      if (hitEnemy) {
-        hitEnemy.hit(bubble.damage);
-        this.soundManager.play("bubblePop");
-        return false;
-      }
-
-      return true;
-    });
-  }
-
-  /**
-   * Checks collisions between character and coins
-   */
-  handleCoinCollisions() {
-    this.coins = this.coins.filter((coin) => {
-      if (this.character.isColliding(coin)) {
-        this.soundManager.play("coinPickup");
-        this.character.coins++;
-        this.coinbar.updateCoin(this.character.coins);
-        return false;
-      }
-
-      return true;
-    });
-  }
-
-  /**
-   * Checks collisions between character and poison bottles
-   */
-  handlePoisonBottlesCollisions() {
-    this.poisonBottles = this.poisonBottles.filter((poisonBottle) => {
-      if (this.character.isColliding(poisonBottle)) {
-        this.soundManager.play("bottlePickup");
-        this.character.poisonBottles++;
-        this.poisonbar.updatePoison(this.character.poisonBottles);
-        return false;
-      }
-
-      return true;
-    });
-  }
-
-  handleFinSlapCollisions() {
-    if (!this.character.isFinSlapAttacking) return;
-
-    if (this.character.finSlapHitDone) return;
-
-    const attackOffset = 10;
-
-    this.enemies.forEach((enemy) => {
-      const overlapsY =
-        this.character.y < enemy.y + enemy.height &&
-        this.character.y + this.character.height > enemy.y;
-
-      const overlapsX = this.character.otherDirection
-        ? this.character.x - attackOffset < enemy.x + enemy.width
-        : this.character.x + this.character.width - attackOffset > enemy.x;
-
-      if (overlapsX && overlapsY) {
-        enemy.finSlapMarked = true;
-
-        this.character.finSlapHitDone = true;
-      }
-    });
-  }
-
   applyFinSlapDamage() {
     this.enemies.forEach((enemy) => {
       if (enemy.finSlapMarked) {
@@ -256,38 +191,71 @@ class World {
   }
 
   /**
-   * Updates the boss health bar if a boss exists
+   * Creates and throws a new projectile.
+   * @param {string} type - Type of bubble
    */
-  updateBossHealthbar() {
-    const boss = this.enemies.find((enemy) => enemy instanceof Endboss);
-
-    if (boss && this.bossHealthbar) {
-      this.bossHealthbar.updateBossHealth(boss.health);
-    }
+  throwObject(type) {
+    const x = this.character.x + (this.character.otherDirection ? -20 : 120);
+    const y = this.character.y + 80;
+    const bubble = new ThrowableObject(
+      x,
+      y,
+      this.character.otherDirection,
+      type,
+    );
+    bubble.throw();
+    this.throwableObjects.push(bubble);
   }
 
   /**
-   * Draws the entire world
+   * Draws the entire world.
    */
   draw() {
-    this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+    this.clearCanvas();
+    this.drawGameObjects();
+    this.drawUI();
+  }
 
+  /**
+   * Clears the canvas.
+   */
+  clearCanvas() {
+    this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+  }
+
+  /**
+   * Draws all game objects affected by camera movement.
+   */
+  drawGameObjects() {
     this.ctx.save();
     this.ctx.translate(this.camera_x, 0);
 
-    this.addObjectToMap(this.backgroundObjects);
-    this.addObjectToMap(this.coins);
-    this.addObjectToMap(this.enemies);
-    this.addObjectToMap(this.poisonBottles);
-    this.addToMap(this.character);
-    this.addObjectToMap(this.throwableObjects);
+    [
+      this.backgroundObjects,
+      this.coins,
+      this.enemies,
+      this.poisonBottles,
+      [this.character],
+      this.throwableObjects,
+    ].forEach((objects) => this.addObjectToMap(objects));
 
     this.ctx.restore();
+  }
 
+  /**
+   * Draws all fixed UI elements.
+   */
+  drawUI() {
     this.addToMap(this.healthbar);
     this.addToMap(this.poisonbar);
     this.addToMap(this.coinbar);
+    this.drawBossHealthbar();
+  }
 
+  /**
+   * Draws the boss health bar after first contact.
+   */
+  drawBossHealthbar() {
     const boss = this.enemies.find((enemy) => enemy instanceof Endboss);
 
     if (boss && boss.hadFirstContact && this.bossHealthbar) {
@@ -296,7 +264,7 @@ class World {
   }
 
   /**
-   * Draws multiple objects
+   * Draws multiple objects.
    * @param {Array} objects - Objects to draw
    */
   addObjectToMap(objects) {
@@ -304,7 +272,7 @@ class World {
   }
 
   /**
-   * Draws one object
+   * Draws one object.
    * @param {Object} mo - Drawable object
    */
   addToMap(mo) {
@@ -326,7 +294,7 @@ class World {
   }
 
   /**
-   * Flips an object horizontally
+   * Flips an object horizontally.
    * @param {Object} mo - Object to flip
    */
   flipImage(mo) {
@@ -337,7 +305,7 @@ class World {
   }
 
   /**
-   * Restores flipped object position
+   * Restores flipped object position.
    * @param {Object} mo - Object to restore
    */
   flipImageBack(mo) {
